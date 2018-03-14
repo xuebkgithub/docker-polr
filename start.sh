8278e64f78af7c778260bb91c3b86ba6c680a889
Aurélien JANVIER <dev@ajanvier.fr>
2018-03-14 17:50:50 +0100

    fixing this APP_KEY problem

--- start.sh
+++ start.sh
@@ -5,9 +5,11 @@ if [ ! -f ".env" ]; then
     POLR_GENERATED_AT=`date +"%B %d, %Y"`
     export POLR_GENERATED_AT
 
+    APP_KEY=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+    export APP_KEY
+
     envsubst < ".env_polr" > ".env"
 
-    php artisan key:generate --force
     php artisan migrate:install --force
     php artisan migrate --force
 fi
