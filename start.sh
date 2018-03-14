6359dc288ab2311a9e1a6c013a040499672d21d4
Aurélien JANVIER <dev@ajanvier.fr>
2018-03-14 14:46:35 +0100

    fixing start.sh script

--- start.sh
+++ start.sh
@@ -2,13 +2,14 @@
 
 if [ ! -f ".env" ]; then
     cd /src
-    
+
     POLR_GENERATED_AT=`date +"%B %d, %Y"`
     export POLR_GENERATED_AT
 
     envsubst < ".env.setup" > ".env"
 
     php artisan key:generate
+    php artisan migrate:install
     php artisan migrate
 fi
 
