c1cc6d5b587badc600007d7846c5c9c7834adc97
ajanvier <dev@ajanvier.fr>
2018-03-14 20:58:50 +0100

    forgot to rename the UserSeeder class to AdminSeeder

--- seeders/AdminSeeder.php
+++ seeders/AdminSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Factories\UserFactory;
 
-class UserSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
